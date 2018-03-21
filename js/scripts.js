01f985d53d23b53586c759ea22528f51916a8ce1
Isaac Shoman and Ted Leary <isaac@>
2018-03-21 15:08:57 -0700

    Add js and jquery functions that print to console.

--- js/scripts.js
+++ js/scripts.js
@@ -1,55 +1,18 @@
 
-// Business (or back-end) logic:
-
-  var add = function(number1, number2) {
-    return number1 + number2;
-  };
-
-  var subtract = function(number1, number2) {
-    return number1 - number2;
-  };
-
-  var divide = function(number1, number2) {
-    return number1 / number2;
-  };
-
-  var multiply = function(number1, number2) {
-    return number1 * number2;
-  };
-
-// Everything below this line is uses interface (or front-end) logic:
-
-$(function() {
-  $("form#add").submit(function(event) {
+// Front-End Logic
+$(document).ready(function() {
+  $("form#form1").submit(function(event) {
+    var name = $("input#name").val();
+    console.log(name)
+    var food = $("input:radio[name=food]:checked").val();
+    console.log(food)
+    var music = $("#music").val();
+    console.log(music)
+    var favoriteColor = $("#color").val();
+    console.log(favoriteColor)
+    var dob = $("#born").val();
+    console.log(dob)
+    alert("Survey Submitted!");
     event.preventDefault();
-    var number1 = parseInt($("#add1").val());
-    var number2 = parseInt($("#add2").val());
-    var result = add(number1, number2);
-    $("#output1").text(result);
-  });
-
-  $("form#sub").submit(function(event) {
-    event.preventDefault();
-    var number1 = parseInt($("#sub1").val());
-    var number2 = parseInt($("#sub2").val());
-    var result = subtract(number1, number2);
-    $("#output2").text(result);
-  });
-
-    $("form#mult").submit(function(event) {
-      event.preventDefault();
-      var number1 = parseInt($("#mult1").val());
-      var number2 = parseInt($("#mult2").val());
-      var result = multiply(number1, number2);
-      $("#output3").text(result);
     });
-
-    $("form#divide").submit(function(event) {
-      event.preventDefault();
-      var number1 = parseInt($("#div1").val());
-      var number2 = parseInt($("#div2").val());
-      var result = divide(number1, number2);
-      $("#output4").text(result);
-    });
-
-  });
+});
